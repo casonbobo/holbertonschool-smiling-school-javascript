839de86a9835fa7d76de17ebdb04d0268b39ca97
casonbobo <118225257+casonbobo@users.noreply.github.com>
2023-12-04 16:10:06 -0600

    pooosh cause its working

--- 1-script.js
+++ 1-script.js
@@ -4,34 +4,34 @@ $('#carouselExampleControls').on('slide.bs.carousel', function (e) {
 });
 
 function loadDoc() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-           
-        }
-    };
-    xhttp.open("GET", "smile-school-api.txt", true);
-    console.log('GET');
-    xhttp.send();
-    console.log('SEND');
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+         
+      }
+  };
+  xhttp.open("GET", "smile-school-api.txt", true);
+  console.log('GET');
+  xhttp.send();
+  console.log('SEND');
 }
 
 let data = [
-    {
-        "id": 1,
-        "pic_url": "https://smileschool-api.s3.amazonaws.com/profile_5.jpg",
-        "name": "John Doe",
-        "title": "Founder of SmileSchool",
-        "text": "Those tutorials are concise and go straight to the point. I can’t think of a better place to learn smiling. And it’s so fun!"
-    },
-    {
-        "id": 2,
-        "pic_url": "https://smileschool-api.s3.amazonaws.com/profile_2.jpg",
-        "name": "Nannie Lawrence",
-        "title": "Best \"little smile\" 2017",
-        "text": "ThoseLorem ipsum dolor sit amet, consect adipiscing elit, sed do eiusmod. Lorem ipsum dolor sit amet, sed do eiusmod."
-    }
-   ];
+  {
+      "id": 1,
+      "pic_url": "https://smileschool-api.s3.amazonaws.com/profile_5.jpg",
+      "name": "John Doe",
+      "title": "Founder of SmileSchool",
+      "text": "Those tutorials are concise and go straight to the point. I can’t think of a better place to learn smiling. And it’s so fun!"
+  },
+  {
+      "id": 2,
+      "pic_url": "https://smileschool-api.s3.amazonaws.com/profile_2.jpg",
+      "name": "Nannie Lawrence",
+      "title": "Best \"little smile\" 2017",
+      "text": "ThoseLorem ipsum dolor sit amet, consect adipiscing elit, sed do eiusmod. Lorem ipsum dolor sit amet, sed do eiusmod."
+  }
+ ];
    
   let items = '<div class="carousel-inner">';
    
